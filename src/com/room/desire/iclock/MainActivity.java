b357775c2bd2806a52d2316315edd0219b44456e
desire <room.desire@gmail.com>
2014-08-07 21:30:53 +0800

    add four fragment.

--- src/com/room/desire/iclock/MainActivity.java
+++ src/com/room/desire/iclock/MainActivity.java
@@ -2,16 +2,33 @@
 package com.room.desire.iclock;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    public static final String TAG_WORLD_CLOCK = "tag_world_clock";
+
+    public static final String TAG_ALARM = "tag_alarm";
+
+    public static final String TAG_STOP_WATCH = "tag_stop_watch";
+
+    public static final String TAG_TIMER = "tag_timer";
+
     private View mWorldClockTab, mAlarmTab, mStopWatchTab, mTimerTab;
+    
+    private ViewGroup mContainer;
+
+    private TextView mTitleEditBtn;
+
+    private TextView mTitleNameTv;
+
+    private ImageView mTitleAddBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,24 +37,21 @@ public class MainActivity extends Activity {
 
         mWorldClockTab = findViewById(R.id.main_tab_world_clock);
         initTab(mWorldClockTab, R.drawable.ic_launcher, R.string.world_clock);
-        
+
         mAlarmTab = findViewById(R.id.main_tab_alarm);
         initTab(mAlarmTab, R.drawable.ic_launcher, R.string.alarm);
-        
+
         mStopWatchTab = findViewById(R.id.main_tab_stop_watch);
         initTab(mStopWatchTab, R.drawable.ic_launcher, R.string.stop_watch);
-        
+
         mTimerTab = findViewById(R.id.main_tab_timer);
         initTab(mTimerTab, R.drawable.ic_launcher, R.string.timer);
+        
+        mContainer = (ViewGroup) findViewById(R.id.main_container);
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    
+    
 
     private void initTab(View tab, int iconId, int tabelId) {
         ImageView icon = (ImageView) tab.findViewById(R.id.main_tab_icon);
@@ -46,4 +60,6 @@ public class MainActivity extends Activity {
         tv.setText(tabelId);
     }
 
+    private void updateSelectedFragment(int i) {
+    }
 }
---
+++ src/com/room/desire/iclock/fragment/AlarmFragment.java
@@ -0,0 +1,27 @@
+package com.room.desire.iclock.fragment;
+
+import com.room.desire.iclock.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class AlarmFragment extends Fragment {
+    protected View mRootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.fragment_alarm, container, false);
+        return mRootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ((TextView) mRootView.findViewById(R.id.test_text)).setText(R.string.alarm);
+    }
+
+}
